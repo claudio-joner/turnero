0d9037cbbb7ba899c346c560b78cec791ccd8df6
Claudio Joner <claujoner@gmail.com>
2024-11-25 06:34:40 -0300

    Agrega API

---
+++ Examen Final 19-02-24/ParcialSln/Api/Controllers/TurnoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParcialApp.Servicios;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurnoController : ControllerBase
+    {
+        //Agregar referencia del proyecto. Agregar>Referencia del Proyecto
+        private IServicio servicio;
+    }
+    
+}
